4fdcffe3348689823a0a11b806a5a276b86afdd5
Alex W3O <alex@w3o.com>
2023-10-30 09:00:19 +0200

    move resolve fn to obj

--- src/DynoLoader.php
+++ src/DynoLoader.php
@@ -12,11 +12,13 @@ class DynoLoader
     public $dynoArrChanged = false; // if the dynoArr differs from what is saved in the file
 
     public const REMOTE_NSMAP_KEY = 'remote-nsmap';
+    public string $vendorDir;
 
     public function __construct(string $vendorDir) {
         if (! DYNO_FILE || !$vendorDir) {
             return;
         }
+        $this->vendorDir = $vendorDir;
 
         $needUpdateNSMap = !\file_exists(DYNO_FILE);
         if (!$needUpdateNSMap) {
@@ -50,95 +52,33 @@ class DynoLoader
                 $this->quickPrepareDynoArr($vendorDir);
             }
             
-            AutoLoader::$optionalObj = new class {
-                public function resolve(string $filePathString, string $classFullName, string $nameSpaceKey): string {
-                    // "fromURL [optional parameters] replaceNameSpace checkFiles"
-                    $i = \strpos($filePathString, ' ');
-                    $j = \strrpos($filePathString, ' ');
-                    if (!($j > $i)) {
-                        return '';
-                    }
-                    $fromURL = \substr($filePathString, 1, $i - 1);
-                    if (!\filter_var($fromURL, \FILTER_VALIDATE_URL)) {
-                        return '';
-                    }
-                    $checkFilesStr = \substr($filePathString, $j + 1);
-
-                    $midStr = \trim(\substr($filePathString, $i, $j - $i));
-                    $midArr = \explode(' ', $midStr);
-
-                    $replaceNameSpaceDir = \array_pop($midArr);
-                    if ($midArr) {
-                        $targetUnpackDir = \array_pop($midArr);
-                    } else {
-                        $targetUnpackDir = $replaceNameSpaceDir;
-                    }
-                    if (\substr($targetUnpackDir, -1) === '*') {
-                        $targetUnpackDir = \substr($targetUnpackDir, 0, -1);
-                    }
-                    $fullTargetPath = AutoLoader::getPathPrefix($targetUnpackDir);
-                    $fullTargetPath = \strtr($fullTargetPath, '\\', '/');
-                    $oneFileMode = (\substr($fullTargetPath, -4) === '.php');
-                    if ($oneFileMode) {
-                        $fullTargetPath = \dirname($fullTargetPath) . '/';
-                    }
-                    if (!$fullTargetPath || (\substr($fullTargetPath, -1) !== '/')) {
-                        throw new \Exception("Incorrect target namespace-folder: '$replaceNameSpaceDir', must specified folder with prefix-char");
-                    }
-                    $lk = \strlen($nameSpaceKey);
-                    $addPath = \substr($classFullName, $lk, \strlen($classFullName) - $lk);
-                    $addPath = $addPath ? \strtr(\substr($addPath, 1), '\\', '/') : \basename($classFullName);
-                    $pkgChkFile = $addPath . '.php';
-                    //$classFile = $fullTargetPath . $pkgChkFile;
-                    $checkFile = $fullTargetPath . $checkFilesStr;
-
-                    if (!\is_file($checkFile)) {
-                        // File not found - try load
-                        if (!\is_dir($fullTargetPath) && !mkdir($fullTargetPath, 0777, true)) {
-                            throw new \Exception("Can't create target path for download package: $fullTargetPath , foor class=$classFullName");
-                        }
-                        $hashSigBaseObj = new \dynoser\hashsig\HashSigBase();
-                        $res = $hashSigBaseObj->getFilesByHashSig(
-                            $fromURL,
-                            $fullTargetPath,
-                            null,  //array $baseURLs
-                            false, //bool $doNotSaveFiles
-                            false  //bool $doNotOverWrite
-                        );
-                        if (empty($res['successArr']) || !empty($res['errorsArr'])) {
-                            throw new \Exception("Download problem for class $classFullName , package url=$fromURL");
-                        }
-                        if (!\in_array($checkFile, $res['successArr'])) {
-                            throw new \Exception("Successful downloaded hashsig-package, but not found target class file: $classFile");
-                        }
-                        if (AutoLoader::$commiterObj) {
-                            AutoLoader::$commiterObj->addFiles($res['successArr'], $nameSpaceKey, $replaceNameSpaceDir, $classFullName);
-                        }
-                    }
-                    return \strtr($replaceNameSpaceDir, '\\', '/');
-                }
-            };
+            AutoLoader::$optionalObj = $this;
         }
 
         // rebuild dyno-cached files if need
         if ($needUpdateNSMap) {
-            if (!\class_exists('dynoser\\autoload\\DynoImporter', false)) {
-                $chkFile = __DIR__ . '/DynoImporter.php';
-                if (\is_file($chkFile)) {
-                    include_once $chkFile;
-                }
-            }
-            if (!\class_exists('dynoser\\autoload\\DynoImporter')) {
-                throw new \Exception("Can't rebuild dyno-maps because not found DynoImporter class");
-            }
-            $dimObj = new DynoImporter('');
-            foreach($this as $k => $v) {
-                $dimObj->$k = $v;
-            }
-            $dimObj->rebuildDynoCache($vendorDir);
-            $this->dynoArr = $dimObj->dynoArr;
-            AutoLoader::$classesArr = $this->dynoArr;
+            $this->nsMapUp();
         }
+    }
+    
+    public function nsMapUp() {
+        if (!\class_exists('dynoser\\autoload\\DynoImporter', false)) {
+            $chkFile = __DIR__ . '/DynoImporter.php';
+            if (\is_file($chkFile)) {
+                include_once $chkFile;
+            }
+        }
+        if (!\class_exists('dynoser\\autoload\\DynoImporter')) {
+            throw new \Exception("Can't rebuild dyno-maps because not found DynoImporter class");
+        }
+        $this->checkCreateDynoDir($this->vendorDir);
+        $dimObj = new DynoImporter('');
+        foreach($this as $k => $v) {
+            $dimObj->$k = $v;
+        }
+        $dimObj->rebuildDynoCache();
+        $this->dynoArr = $dimObj->dynoArr;
+        AutoLoader::$classesArr = $this->dynoArr;        
     }
     
     public function quickPrepareDynoArr($vendorDir) {
@@ -253,5 +193,75 @@ class DynoLoader
             $this->dynoNSmapFile = $chkDir . '/nsmap.php';
         }
         return $this->dynoDir;
+    }
+    
+    public function resolve(string $filePathString, string $classFullName, string $nameSpaceKey): string {
+        // "fromURL [optional parameters] replaceNameSpace checkFiles"
+        $i = \strpos($filePathString, ' ');
+        $j = \strrpos($filePathString, ' ');
+        if (!($j > $i)) {
+            return '';
+        }
+        $fromURL = \substr($filePathString, 1, $i - 1);
+        if (!\filter_var($fromURL, \FILTER_VALIDATE_URL)) {
+            return '';
+        }
+        $checkFilesStr = \substr($filePathString, $j + 1);
+
+        $midStr = \trim(\substr($filePathString, $i, $j - $i));
+        $midArr = \explode(' ', $midStr);
+
+        $replaceNameSpaceDir = \array_pop($midArr);
+        if ($midArr) {
+            $targetUnpackDir = \array_pop($midArr);
+        } else {
+            $targetUnpackDir = $replaceNameSpaceDir;
+        }
+        if (\substr($targetUnpackDir, -1) === '*') {
+            $targetUnpackDir = \substr($targetUnpackDir, 0, -1);
+        }
+        $fullTargetPath = AutoLoader::getPathPrefix($targetUnpackDir);
+        $fullTargetPath = \strtr($fullTargetPath, '\\', '/');
+        $oneFileMode = (\substr($fullTargetPath, -4) === '.php');
+        if ($oneFileMode) {
+            $fullTargetPath = \dirname($fullTargetPath) . '/';
+        }
+        if (!$fullTargetPath || (\substr($fullTargetPath, -1) !== '/')) {
+            throw new \Exception("Incorrect target namespace-folder: '$replaceNameSpaceDir', must specified folder with prefix-char");
+        }
+        $lk = \strlen($nameSpaceKey);
+        $addPath = \substr($classFullName, $lk, \strlen($classFullName) - $lk);
+        $addPath = $addPath ? \strtr(\substr($addPath, 1), '\\', '/') : \basename($classFullName);
+        $pkgChkFile = $addPath . '.php';
+        //$classFile = $fullTargetPath . $pkgChkFile;
+        $checkFile = $fullTargetPath . $checkFilesStr;
+
+        if (!\is_file($checkFile)) {
+            // File not found - try load
+            if (!\is_dir($fullTargetPath) && !mkdir($fullTargetPath, 0777, true)) {
+                throw new \Exception("Can't create target path for download package: $fullTargetPath , foor class=$classFullName");
+            }
+            $hashSigBaseObj = new \dynoser\hashsig\HashSigBase();
+            $res = $hashSigBaseObj->getFilesByHashSig(
+                $fromURL,
+                $fullTargetPath,
+                null,  //array $baseURLs
+                false, //bool $doNotSaveFiles
+                false  //bool $doNotOverWrite
+            );
+            if (empty($res['successArr']) || !empty($res['errorsArr'])) {
+                throw new \Exception("Download problem for class $classFullName , package url=$fromURL");
+            }
+            if (!\in_array($checkFile, $res['successArr'])) {
+                throw new \Exception("Successful downloaded hashsig-package, but not found target class file: $classFile");
+            }
+            if (AutoLoader::$commiterObj) {
+                AutoLoader::$commiterObj->addFiles($res['successArr'], $nameSpaceKey, $replaceNameSpaceDir, $classFullName);
+            }
+            if (isset($res['successArr']['nsmap.helml'])) {
+                $this->nsMapUp();
+            }
+        }
+        return \strtr($replaceNameSpaceDir, '\\', '/');
     }
 }
